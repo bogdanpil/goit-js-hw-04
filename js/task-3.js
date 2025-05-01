047fe4a65bc7f83d1aabe0c729c997cf14897739
Bogdan <pilipaka@gmail.com>
2025-05-01 17:45:18 +0300

    add all tasks, done all hw tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,24 @@
-//Width of the element
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    return contentWidth + paddingWidth * 2 + borderWidth * 2;
+//Задача 3. Профіль гравця
+
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername: function (newName) {
+        this.username = newName;
+    },
+    updatePlayTime: function (hours) {
+        this.playTime += hours;
+    },
+    getInfo: function () {
+        return `${this.username} has ${this.playTime} active hours!`
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
